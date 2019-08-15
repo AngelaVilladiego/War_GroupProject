c9f1677e9085497ad031c4bf4ef9d7481bb29b64
Angela Villadiego <angie.vdiego@gmail.com>
2019-08-15 15:44:44 -0400

    new class to handle starting the games

---
+++ src/project/PlayWar.java
@@ -0,0 +1,13 @@
+/*
+ * Class with main method that starts the game and initiates the play method 
+ * after user has chosen to play either single or multiplayer
+ */
+package project;
+
+/**
+ *
+ * @author Angela
+ */
+public class PlayWar {
+    
+}
