0ca8fc5693d7e449ddf4428db519304ece2da6df
Angela Villadiego <angie.vdiego@gmail.com>
2019-08-16 18:42:29 -0400

    Finished logic for match looping in play() mathod

--- src/project/WarGameSingle.java
+++ src/project/WarGameSingle.java
@@ -62,18 +62,22 @@ public class WarGameSingle extends WarGame {
         
         //repeat match outline until each player is on last card (2 cards left)
         do {
+            stake = 0;
+            
             showScorecard();
             
             System.out.println("Press enter to flip your card!");
             in.nextLine();
             
-            //give user and computer their cards
+            //give user and computer their cards, up stakes by 2 cards
             player1.setCard(deck.pickCard());
             player2.setCard(deck.pickCard());
             
+            stake += 2;
+            
             //display user's card then computer's card
-            System.out.println(player1.getName() + " flipped a " + player1.getCard().toString() + "!");
-            System.out.println(player2.getName() + " flipped a " + player2.getCard().toString() + "!");
+            System.out.println(player1.getName() + " flipped the " + player1.getCard().toString() + "!");
+            System.out.println(player2.getName() + " flipped the " + player2.getCard().toString() + "!");
             
             //compare cards
             int result = player1.getCard().compareTo(player2.getCard());
@@ -85,13 +89,23 @@ public class WarGameSingle extends WarGame {
             } else if (result < 0) {
                 //<0 means player1's card was lower
                 declareMatchWinner(player2);
+                
+                //give winner points, take away loser's
+                player2.setScore(player2.getScore() + stake);
+                player1.setScore(player1.getScore() - stake);
+                
             } else {
                 //last option is >0, where player1's card was higher
                 declareMatchWinner(player1);
+                
+                //give winner points, take away loser's
+                player2.setScore(player2.getScore() - stake);
+                player1.setScore(player1.getScore() + stake);
             }
             
-            break;
-        } while (deck.cardsLeft() > 2);
+            System.out.println("\nPress enter to continue.");
+            in.nextLine();
+        } while ((deck.cardsLeft() / 2) > 2);
     }
     
     @Override
